ed098c36358a7ef82c252adae120d619bd7fba22
AryanCCTech <aryan.yadav@cctech.co.in>
2024-10-12 01:14:26 +0530

    Update Writer.cpp
    Updates after final review

--- Source Files/Writer.cpp
+++ Source Files/Writer.cpp
@@ -1,16 +1,18 @@
-#include <fstream>
-#include <iostream>
-#include <string>
 #include "Writer.h"
+#include <fstream>
+
 using namespace std;
+
 void Writer::write(vector<Triangle> triangles_list,vector<double> unique_points) {
+	
 	string file = "output.dat";
 	ofstream outfile(file);
+
 	for (auto i : triangles_list) {
-		outfile << unique_points[i.get_p1_x_coord()] << " " << unique_points[i.get_p1_y_coord()] << " " << unique_points[i.get_p1_z_coord()] << endl;
-		outfile << unique_points[i.get_p2_x_coord()] << " " << unique_points[i.get_p2_y_coord()] << " " << unique_points[i.get_p2_z_coord()] << endl;
-		outfile << unique_points[i.get_p3_x_coord()] << " " << unique_points[i.get_p3_y_coord()] << " " << unique_points[i.get_p3_z_coord()] << endl;
-		outfile << unique_points[i.get_p1_x_coord()] << " " << unique_points[i.get_p1_y_coord()] << " " << unique_points[i.get_p1_z_coord()] << endl;
+		outfile << unique_points[i.get_p1_point().get_x_coord()] << " " << unique_points[i.get_p1_point().get_y_coord()] << " " << unique_points[i.get_p1_point().get_z_coord()] << endl;
+		outfile << unique_points[i.get_p2_point().get_x_coord()] << " " << unique_points[i.get_p2_point().get_y_coord()] << " " << unique_points[i.get_p2_point().get_z_coord()] << endl;
+		outfile << unique_points[i.get_p3_point().get_x_coord()] << " " << unique_points[i.get_p3_point().get_y_coord()] << " " << unique_points[i.get_p3_point().get_z_coord()] << endl;
+		outfile << unique_points[i.get_p1_point().get_x_coord()] << " " << unique_points[i.get_p1_point().get_y_coord()] << " " << unique_points[i.get_p1_point().get_z_coord()] << endl;
 		outfile << endl;
 		outfile << endl;
 	}
