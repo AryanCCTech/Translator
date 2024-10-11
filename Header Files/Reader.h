c7721397693bb744e572b8ddd53854a4ea37f813
AryanCCTech <aryan.yadav@cctech.co.in>
2024-10-12 01:15:43 +0530

    Update Reader.h
    Updates after final review

--- Header Files/Reader.h
+++ Header Files/Reader.h
@@ -1,16 +1,18 @@
 #pragma once
-#include "Point.h"
+
 #include "Triangle.h"
 #include <fstream>
 #include <string>
 #include <unordered_map>
 #include <vector>
 #include <sstream>
+
 using namespace std;
+
 class Reader {
-private:
-    string file;
-public:
-    vector<Point> read_stl_file();
-    vector<double> get_unique_points();
+    private:
+        string file;
+    public:
+        vector<Point> read_stl_file();
+        vector<double> get_unique_points();
 };
