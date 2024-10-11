1dd08716d22105b99f9cae533d70382b85fe7361
AryanCCTech <aryan.yadav@cctech.co.in>
2024-10-12 01:11:51 +0530

    Update Point.cpp
    Final updates after review

--- Source Files/Point.cpp
+++ Source Files/Point.cpp
@@ -1,15 +1,16 @@
-#include <iostream>
 #include "Point.h"
 #include "Triangulation.h"
+
 using namespace std;
+
 Point::Point(int x, int y, int z) :x(x), y(y), z(z){}
 Point::~Point() {};
-int Point::get_x_coord() {
+int Point::get_x_coord() const{
     return x;
 }
-int Point::get_y_coord() {
+int Point::get_y_coord() const{
     return y;
 }
-int Point::get_z_coord() {
+int Point::get_z_coord() const{
     return z;
 }
